a82d3f194633c7296c6f7287025b52d4950cc27e
HuRuilizhen <huruilizhen@gmail.com>
2025-11-09 15:45:20 +0800

    feat(fs): add path parser

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,10 @@
 use clap::Parser;
+use colored::Colorize;
+use std::{
+    fs,
+    path::{Path, PathBuf},
+    process::exit,
+};
 
 #[derive(Parser)]
 #[command(
@@ -21,19 +27,52 @@ pub struct Args {
         short,
         long,
         default_value = ".",
-        help = "Directory to be distrubute, e.g. '~/desktop/temp'"
+        help = "path to be distributed can be a file or a directory., e.g. '~/desktop/temp'"
     )]
-    dir: String,
+    path: String,
 
-    #[arg(short, long, default_value = "80", help = "port used by the service")]
+    #[arg(long, default_value = "80", help = "port used by the service")]
     port: i32,
 }
 
-pub fn parse() -> (String, i32) {
+fn parse_path(path: String) -> Result<Vec<PathBuf>, String> {
+    let path: &Path = Path::new(&path);
+
+    if !path.exists() {
+        return Err("given path is invalid".to_string());
+    }
+
+    let mut files: Vec<PathBuf> = Vec::new();
+
+    if !path.is_dir() {
+        files.push(path.to_path_buf());
+        return Ok(files);
+    }
+
+    for entry in fs::read_dir(path).unwrap() {
+        let entry = entry.unwrap().path();
+        if entry.is_dir() {
+            continue;
+        }
+
+        files.push(entry);
+    }
+
+    Ok(files)
+}
+
+pub fn parse() -> (Vec<PathBuf>, i32) {
     let args = Args::parse();
 
-    let dir = args.dir;
+    let files = match parse_path(args.path) {
+        Ok(files) => files,
+        Err(err) => {
+            println!("{}: {err}", "error".red().bold());
+            exit(1);
+        }
+    };
+
     let port = args.port;
 
-    (dir, port)
+    (files, port)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use crate::cli::parse;
-
+use std::path::PathBuf;
 mod cli;
 
 fn main() {
-    let (dir, port): (String, i32) = parse();
-    println!(r"{dir}, {port}");
+    let (files, _): (Vec<PathBuf>, i32) = parse();
+    for file in files {
+        println!("[FILE] {}", file.file_name().unwrap().to_str().unwrap());
+    }
 }
