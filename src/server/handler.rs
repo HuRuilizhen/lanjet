16d0e8dda755f08e127f6011191895028f56c497
HuRuilizhen <huruilizhen@gmail.com>
2025-11-10 23:30:15 +0800

    feat(server): complete basic endpoint

--- src/server/handler.rs
+++ src/server/handler.rs
@@ -1,14 +1,45 @@
 use super::AppState;
+use axum::Json;
+use axum::body::Body;
 use axum::extract::Path as AxumPath;
 use axum::extract::State as AxumState;
+use axum::http::{StatusCode, header};
+use axum::response::{IntoResponse, Response};
+use serde_json::json;
+use std::path::PathBuf;
+use tokio_util::io::ReaderStream;
 
-pub async fn list_files(AxumState(app_state): AxumState<AppState>) -> String {
-    "receive list files request".to_string()
+pub async fn list_files(AxumState(app_state): AxumState<AppState>) -> impl IntoResponse {
+    Json(json!(app_state.path_set))
 }
 
 pub async fn download_file(
     AxumState(app_state): AxumState<AppState>,
     AxumPath(path): AxumPath<String>,
-) -> String {
-    format!("receive download file {} request", path).to_string()
+) -> impl IntoResponse {
+    if !app_state.path_set.contains(&path) {
+        return (StatusCode::NOT_FOUND, "File not found".to_string()).into_response();
+    }
+
+    let path_buf = PathBuf::from(&path);
+    let absolute_path = app_state.base_dir.join(&path_buf);
+
+    let file = match tokio::fs::File::open(&absolute_path).await {
+        Ok(f) => f,
+        Err(_) => {
+            return (StatusCode::INTERNAL_SERVER_ERROR, "Could not open file").into_response();
+        }
+    };
+
+    let stream = ReaderStream::new(file);
+    let body = Body::from_stream(stream);
+
+    Response::builder()
+        .header(header::CONTENT_TYPE, "application/octet-stream")
+        .header(
+            header::CONTENT_DISPOSITION,
+            format!("attachment; filename=\"{}\"", path),
+        )
+        .body(body)
+        .unwrap()
 }
--- src/server/state.rs
+++ src/server/state.rs
@@ -6,22 +6,20 @@ use std::path::PathBuf;
 #[derive(Debug, Clone)]
 pub struct AppState {
     pub base_dir: PathBuf,
-    pub path_set: HashSet<PathBuf>,
-    pub files: Vec<PathBuf>,
+    pub path_set: HashSet<String>,
 }
 
 impl From<ServerContext> for AppState {
     fn from(server_context: ServerContext) -> Self {
-        let mut path_set: HashSet<PathBuf> = HashSet::new();
+        let mut path_set: HashSet<String> = HashSet::new();
 
         for file_path in &server_context.files {
             let relative_path: PathBuf = diff_paths(file_path, &server_context.base_dir).unwrap();
-            path_set.insert(relative_path);
+            path_set.insert(relative_path.display().to_string());
         }
 
         Self {
             base_dir: server_context.base_dir,
-            files: server_context.files,
             path_set: path_set,
         }
     }
