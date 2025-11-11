cda2ca85935e4f521c5a71c8d6d43fb30534ba9b
HuRuilizhen <huruilizhen@gmail.com>
2025-11-12 03:12:55 +0800

    style(server): make fmt happy

--- src/server/handler.rs
+++ src/server/handler.rs
@@ -1,10 +1,10 @@
 use super::AppState;
-use axum::Json;
 use axum::body::Body;
 use axum::extract::Path as AxumPath;
 use axum::extract::State as AxumState;
-use axum::http::{StatusCode, header};
+use axum::http::{header, StatusCode};
 use axum::response::{IntoResponse, Response};
+use axum::Json;
 use serde_json::json;
 use std::path::PathBuf;
 use tokio_util::io::ReaderStream;
